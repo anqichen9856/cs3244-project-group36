34102780c26311692229de565d803d1b03e80a13
anqichen9856 <chenanqi9856@gmail.com>
2021-03-29 12:24:53 +0800

    finish calculating bus-related features

--- nearest_bus_terminals.R
+++ nearest_bus_terminals.R
@@ -2,8 +2,7 @@ library(dplyr)
 library(geosphere)
 
 bus <- read.csv("data_geocoded/bus.csv", stringsAsFactors = FALSE)
-hdb <- read.csv("data_geocoded/hdb.csv", stringsAsFactors = FALSE)[1:100,]
-
+hdb <- read.csv("data_geocoded/data_with_coor.csv", stringsAsFactors = FALSE)
 findNearestDistance <- function(hdb, facility) {
   hdb_coord <- hdb %>% select(lon,lat)
   facility_coord <- facility %>% select(lon,lat)
@@ -23,12 +22,11 @@ output <- data.frame()
 
 for(i in 1:nrow(hdb)){
   hdb_entry <- hdb[i,]
-  dist_nearest_bus_stop <- findNearestDistance(hdb_entry, bus)
-  bus_stops_within_500m <- findCountWithinDistance(hdb_entry, bus, 500)
-  bus_stops_within_2000m <- findCountWithinDistance(hdb_entry, bus, 2000)
-  row <- as.data.frame(cbind(dist_nearest_bus_stop, bus_stops_within_500m, bus_stops_within_2000m))
+  dist_nearest_bus_terminal <- findNearestDistance(hdb_entry, bus)
+  bus_terminals_within_500m <- findCountWithinDistance(hdb_entry, bus, 500)
+  bus_terminals_within_800m <- findCountWithinDistance(hdb_entry, bus, 800)
+  bus_terminals_within_1000m <- findCountWithinDistance(hdb_entry, bus, 1000)
+  row <- as.data.frame(cbind(dist_nearest_bus_terminal, bus_terminals_within_500m, bus_terminals_within_800m, bus_terminals_within_1000m))
   output <- rbind(output, row)
 }
-View(output)
-
-
+write.csv(output, 'feature_bus_terminals.csv', row.names = FALSE)
