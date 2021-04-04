ed07a5f3a2a4f4540b2a850ed837aa4c0ced0178
Chen Su <chensu@comp.nus.edu.sg>
2021-04-05 00:42:28 +0800

    Modified a few hyperparams

--- neural_network.py
+++ neural_network.py
@@ -12,6 +12,12 @@ from sklearn.metrics import f1_score
 from keras.models import Sequential
 from keras.layers import Dense
 
+from numpy.random import seed
+from tensorflow import set_random_seed
+
+seed(0)
+set_random_seed(0)
+
 def preprocessing_X(mtx):
     scaler_x = MinMaxScaler()
     scaler_x.fit(mtx)
@@ -22,8 +28,23 @@ def preprocessing_Y(mtx):
     scaler_y.fit(mtx)
     return scaler_y, scaler_y.transform(mtx)
 
+def build_model():
+    model = Sequential()
+    model.add(Dense(19, input_dim=19, kernel_initializer='normal',activation='relu'))
+    model.add(Dense(8, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='linear'))    
+    model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
+    return model
+
 def train_model(X, y, model):
-    model.fit(X, y, epochs=10, steps_per_epoch=100)
+    # param_grid = dict(filters=[256, 512, 1024],
+    #               kernel_size=[2, 3],
+    #               pool_size=[2, 3],
+    #               dropout_rate=[0.15, 0.2], 
+    #               n_dense_1=[512], 
+    #               n_dense_2=[256], 
+    #               n_dense_3=[128])
+    model.fit(X, y, epochs=50, batch_size = len(X), verbose=1, shuffle=False)
 
 def predict(features, model):
     sc = StandardScaler()
@@ -34,34 +55,37 @@ def get_score(y_pred, y_val):
     return mean_squared_error(y_pred/100000, y_val/100000)
 
 def main():
+    # read data for train
     train = pd.read_csv('data_for_model/training_data.csv')
     LE = LabelEncoder()
     train['town'] = LE.fit_transform(train['town']) 
     train['flat_model'] = LE.fit_transform(train['flat_model'])
-    labels = train.iloc[:5000,19:20].values
-    features = train.iloc[:5000,:19].values
+    labels = train.iloc[:10000,19:20].values
+    features = train.iloc[:10000,:19].values
 
+    # preprocess training data
     X_train = preprocessing_X(features)
     scaler_y, y_train = preprocessing_Y(labels)
 
-    model = Sequential()
-    model.add(Dense(19, input_dim=19, activation='relu'))
-    model.add(Dense(2000, activation='relu'))
-    model.add(Dense(1, activation='linear'))    
-    model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
-
+    # build and train model
+    model = build_model()
     train_model(X_train, y_train, model)
     
+    # read in validation data
     validation = pd.read_csv('data_for_model/validation_data.csv')
     validation['town'] = LE.fit_transform(validation['town']) 
     validation['flat_model'] = LE.fit_transform(validation['flat_model'])
     labels_val = validation.iloc[:,19:20].values
     features_val = validation.iloc[:,:19].values
 
+    # preprocess validation data
     X_val = preprocessing_X(features_val)
     scaler_y, y_val = preprocessing_Y(labels_val)
 
+    # predict y values for validation data
     val_res = scaler_y.inverse_transform(predict(X_val, model))
+
+    # get performance score on validation data
     score = get_score(val_res, labels_val)
     print('score on validation = {}'.format(score))
 
