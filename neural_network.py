0a3b6585760de27290d5dfd08120fcbde6d61f60
Chen Su <chensu@comp.nus.edu.sg>
2021-04-05 21:44:46 +0800

    changed to model to sue LabelBinarizer and price_per_sqm with k-fold validation.

--- neural_network.py
+++ neural_network.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
@@ -37,28 +37,37 @@ def build_model():
     model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
     return model
 
-def predict(features, model):
-    # sc = StandardScaler()
-    # X = sc.fit_transform(features)
-    return model.predict(features)
-
 def get_score(y_pred, y_val):
     return mean_squared_error(y_pred/100000, y_val/100000)
 
 def main():
     # read data for train
-    train = pd.read_csv('data_for_model/training_data.csv')
-    LE = LabelEncoder()
-    train['town'] = LE.fit_transform(train['town']) 
-    train['flat_model'] = LE.fit_transform(train['flat_model'])
-    labels = train.iloc[:,19:20].values
+    train = pd.read_csv('data_for_model/new_with_price_per_sqm/training_data.csv')
+    LB = LabelBinarizer()
+    train['town'] = LB.fit_transform(train['town']) 
+    train['flat_model'] = LB.fit_transform(train['flat_model'])
+    labels = train.iloc[:,20:].values
+    total_price = train.iloc[:,19:20].values
     features = train.iloc[:,:19].values
+    floor_area = np.asarray(train['floor_area_sqm'].values).reshape(len(labels),1)
 
     # preprocess training data
     X_train = preprocessing_X(features)
     scaler_y, y_train = preprocessing_Y(labels)
 
     # build and train model
+    cv = RepeatedKFold(n_splits=5, n_repeats=1, random_state=0)
+    cvscores = []
+    for train, test in cv.split(X_train,y_train):
+        model = build_model()
+        result = model.fit(X_train[train], y_train[train], epochs=100, batch_size = len(X_train[train]), verbose=1, shuffle=False)
+        val_res = scaler_y.inverse_transform(model.predict(X_train[test]))
+        # print(val_res[0:10], labels[test][0:10])
+        # print(floor_area)
+        score = get_score(val_res * floor_area[test], total_price[test])
+        cvscores.append(score)
+    print(cvscores)
+    
     # model = KerasClassifier(build_fn=build_model, verbose=1)
     # param_grid = dict(epochs=[20,50,100],
     #               batch_size=[1000,5000,len(X_train)],
@@ -67,40 +76,34 @@ def main():
     #               init= ['glorot_uniform', 'normal', 'uniform']
     #             #   penalty=['l1', 'l2']
     #             )
-    cv = RepeatedKFold(n_splits=5, n_repeats=1, random_state=0)
-    cvscores = []
     # search = GridSearchCV(estimator=model, param_grid=param_grid, scoring='neg_root_mean_squared_error', n_jobs=-1, cv=cv)
-    for train, test in cv.split(X_train,y_train):
-        model = build_model()
-        result = model.fit(X_train[train], y_train[train], epochs=100, batch_size = len(X_train[train]), verbose=1, shuffle=False)
-        val_res = scaler_y.inverse_transform(predict(X_train[test], model))
-        print(val_res[0:10], labels[test][0:10])
-        score = get_score(val_res, labels[test])
-        cvscores.append(score)
-    
-    print(cvscores)
 
-    # summarize result
-    # print('Best Score: %s' % result.best_score_)
-    # print('Best Hyperparameters: %s' % result.best_params_)
+    # for train, test in cv.split(X_train,y_train):
+    #     model = build_model()
+    #     result = model.fit(X_train[train], y_train[train], epochs=100, batch_size = len(X_train[train]), verbose=1, shuffle=False)
+    #     val_res = scaler_y.inverse_transform(predict(X_train[test], model))
+    #     print(val_res[0:10], labels[test][0:10])
+    #     score = get_score(val_res, labels[test])
+    #     cvscores.append(score)
     
-    # read in validation data
-    # validation = pd.read_csv('data_for_model/validation_data.csv')
-    # validation['town'] = LE.fit_transform(validation['town']) 
-    # validation['flat_model'] = LE.fit_transform(validation['flat_model'])
-    # labels_val = validation.iloc[:,19:20].values
-    # features_val = validation.iloc[:,:19].values
 
-    # # preprocess validation data
+    # # read in test data
+    # test = pd.read_csv('data_for_model/test_data.csv')
+    # test['town'] = LE.fit_transform(test['town']) 
+    # test['flat_model'] = LE.fit_transform(test['flat_model'])
+    # labels_val = test.iloc[:,19:20].values
+    # features_val = test.iloc[:,:19].values
+
+    # # preprocess test data
     # X_val = preprocessing_X(features_val)
     # scaler_y, y_val = preprocessing_Y(labels_val)
 
-    # # predict y values for validation data
+    # # predict y values for test data
     # val_res = scaler_y.inverse_transform(predict(X_val, search))
 
-    # # get performance score on validation data
+    # # get performance score on test data
     # score = get_score(val_res, labels_val)
-    # print('score on validation = {}'.format(score))
+    # print('score on test = {}'.format(score))
 
 if __name__ == "__main__":
     main()
