1bd8ff0ba5f71c84dae4c7ef1b6973b56ce60b94
Chen Su <chensu@comp.nus.edu.sg>
2021-04-16 01:01:40 +0800

    updated NN. now it gives better performance than RF....

--- neural_network_best_params.py
+++ neural_network_best_params.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 import pandas as pd
 
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.model_selection import RepeatedKFold,GridSearchCV
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from keras.wrappers.scikit_learn import KerasClassifier
@@ -18,7 +18,7 @@ tf.random.set_seed(0)
 def preprocessing_X(mtx):
     scaler_x = StandardScaler()
     scaler_x.fit(mtx)
-    return scaler_x.transform(mtx)
+    return scaler_x, scaler_x.transform(mtx)
 
 def preprocessing_Y(mtx):
     scaler_y = StandardScaler()
@@ -27,10 +27,18 @@ def preprocessing_Y(mtx):
 
 def build_model():
     model = Sequential()
-    model.add(Dense(100, input_dim=19, kernel_initializer='normal',activation='selu'))
-    model.add(Dense(90, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(80, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(128, input_dim=63, kernel_initializer='normal',activation='selu'))
+    model.add(Dense(64, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(32, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='orthogonal', activation='linear'))    
+    model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
+    return model
+
+def build_simple_model():
+    model = Sequential()
+    model.add(Dense(32, input_dim=63, kernel_initializer='normal',activation='relu'))
+    model.add(Dense(16, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal', activation='linear'))    
     model.compile(loss='mse', optimizer='adam', metrics=['mse','mae'])
     return model
 
@@ -66,58 +74,93 @@ def get_rmse_score(y_pred, y_val):
     return mean_squared_error(y_pred/10000, y_val/10000, squared=False)
 
 def main():
-    # read data for train
+    # read data
     train = pd.read_csv('data_for_model/new_with_price_per_sqm/training_data.csv')
-    LE = LabelEncoder()
-    train['town'] = LE.fit_transform(train['town']) 
-    train['flat_model'] = LE.fit_transform(train['flat_model'])
-    labels = train.iloc[:,20:].values
-    total_price = train.iloc[:,19:20].values
-    features = train.iloc[:,:19].values
+    test = pd.read_csv('data_for_model/new_with_price_per_sqm/test_data.csv')
+    
+    LE1 = LabelEncoder()
+    train['town'] = LE1.fit_transform(train['town']) 
+    test['town'] = LE1.transform(test['town']) 
+
+    LE2 = LabelEncoder()
+    train['flat_model'] = LE2.fit_transform(train['flat_model'])
+    test['flat_model'] = LE2.transform(test['flat_model'])
+
+    OHE1 = OneHotEncoder(handle_unknown='ignore')
+    l1 = pd.DataFrame(OHE1.fit_transform(train[['town']]).toarray())
+    train = train.join(l1)
+    l2 = pd.DataFrame(OHE1.transform(test[['town']]).toarray())
+    test = test.join(l2)
+
+    OHE2 = OneHotEncoder(handle_unknown='ignore')
+    l3 = pd.DataFrame(OHE2.fit_transform(train[['flat_model']]).toarray())
+    l3.columns = list(range(26,46))
+    train = train.join(l3)
+    l4 = pd.DataFrame(OHE2.transform(test[['flat_model']]).toarray())
+    l4.columns = list(range(26,46))
+    test = test.join(l4)
+
+    labels = train['price_per_sqm'].values
+    labels = np.asarray(labels).reshape(len(labels),1)
+    total_price = np.asarray(train['resale_price'].values).reshape(len(labels),1)
+    features = train.drop(columns=['town', 'flat_model','resale_price','price_per_sqm'])
     floor_area = np.asarray(train['floor_area_sqm'].values).reshape(len(labels),1)
 
     # preprocess training data
-    X_train = preprocessing_X(features)
-    scaler_y_train, y_train = preprocessing_Y(labels)
+    scaler_x, X_train = preprocessing_X(features)
+    scaler_y, y_train = preprocessing_Y(labels)
 
     # read in test data
-    test = pd.read_csv('data_for_model/new_with_price_per_sqm/test_data.csv')
-    test['town'] = LE.fit_transform(test['town']) 
-    test['flat_model'] = LE.fit_transform(test['flat_model'])
-    labels_test = test.iloc[:,20:].values
-    total_price_test = test.iloc[:,19:20].values
-    features_test = test.iloc[:,:19].values
+    
+    labels_test = test['price_per_sqm'].values
+    labels_test = np.asarray(labels_test).reshape(len(labels_test),1)
+    total_price_test = np.asarray(test['resale_price'].values).reshape(len(labels_test),1)
+    features_test = test.drop(columns=['town', 'flat_model','resale_price','price_per_sqm'])
     floor_area_test = np.asarray(test['floor_area_sqm'].values).reshape(len(labels_test),1)
 
     # preprocess test data
-    X_test = preprocessing_X(features_test)
-    scaler_y_test, y_test = preprocessing_Y(labels_test)
+    X_test = scaler_x.transform(features_test)
+    y_test = scaler_y.transform(labels_test)
 
     # fine_tune
     # fine_tune(X_train, y_train, scaler_y, floor_area, total_price)
     
     # train on all training data with best hyper-params
-    model = build_model()
-    result = model.fit(X_train, y_train, epochs=400, batch_size = int(len(X_train)/256), verbose=1, shuffle=False)
+    # model = build_model()
+    model = build_simple_model()
+    # result = model.fit(X_train, y_train, epochs=300, batch_size=256, verbose=1, shuffle=False)
+    result = model.fit(X_train, y_train, epochs=100)
 
     # get scores for validation
     # y_valiadation = model.predict(train_dmatrix).reshape(len(labels),1)
-    y_valiadation = scaler_y_train.inverse_transform(model.predict(X_train))
-    mae = get_mae_score(y_valiadation * floor_area, total_price)    
+    y_valiadation = scaler_y.inverse_transform(model.predict(X_train))
+    mae = get_mae_score(y_valiadation * floor_area, total_price)
     print('mae score on validation = {}'.format(mae))
     mse = get_mse_score(y_valiadation * floor_area, total_price)
     print('mse score on validation = {}'.format(mse))
     rmse = get_rmse_score(y_valiadation * floor_area, total_price)
     print('rmse score on validation = {}'.format(rmse))
+    # mae = get_mae_score(y_valiadation, total_price)
+    # print('mae score on validation = {}'.format(mae))
+    # mse = get_mse_score(y_valiadation, total_price)
+    # print('mse score on validation = {}'.format(mse))
+    # rmse = get_rmse_score(y_valiadation, total_price)
+    # print('rmse score on validation = {}'.format(rmse))
     
     # get score for test 
-    y_pred = scaler_y_test.inverse_transform(model.predict(X_test))
+    y_pred = scaler_y.inverse_transform(model.predict(X_test))
     mae = get_mae_score(y_pred * floor_area_test, total_price_test)    
     print('mae score on test = {}'.format(mae))
     mse = get_mse_score(y_pred * floor_area_test, total_price_test)
     print('mse score on test = {}'.format(mse))
     rmse = get_rmse_score(y_pred * floor_area_test, total_price_test)
     print('rmse score on test = {}'.format(rmse))
+    # mae = get_mae_score(y_pred, total_price_test)    
+    # print('mae score on test = {}'.format(mae))
+    # mse = get_mse_score(y_pred, total_price_test)
+    # print('mse score on test = {}'.format(mse))
+    # rmse = get_rmse_score(y_pred, total_price_test)
+    # print('rmse score on test = {}'.format(rmse))
     
 
     '''
